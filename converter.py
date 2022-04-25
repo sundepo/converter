06ff8b535d7ee21146ac38ff8c3f52abdcb8b41f
sundepo <kotoff-den777@yandex.ru>
2022-04-25 23:30:57 +0300

    Add: Support for command line arguments

--- converter.py
+++ converter.py
@@ -1,1 +1,18 @@
-# TBD
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert hex to bin or bin to hex')
+parser.add_argument(
+    '--hex2bin',
+    type=str,
+    default=None,
+    help='Enter a hexadecimal number'
+)
+parser.add_argument(
+    '--bin2hex',
+    type=str,
+    default=None,
+    help='Enter a binary number'
+)
+args = parser.parse_args()
+print(args.hex2bin)
+print(args.bin2hex)
